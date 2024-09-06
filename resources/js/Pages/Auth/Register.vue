b4d079bb4e0f1f939e9ca3dbf837ff6211c68f07
Eliander Gomes <49985115+ElianderG@users.noreply.github.com>
2024-09-06 09:23:29 -0300

    register form logic

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -4,6 +4,23 @@ import Title from '../../Components/title.vue';
 import TextLink from '../../Components/TextLink.vue';
 import inputField from '../../Components/inputField.vue';
 import PrimaryBtn from '../../Components/PrimaryBtn.vue';
+import ErrorMessages from '../../Components/ErrorMessages.vue';
+import { useForm } from '@inertiajs/vue3';
+
+
+const form = useForm({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+});
+
+const submit = () => {
+    form.post(route('register'), {
+        onFinish: () => form.reset('password', 'password_confirmation'),
+    })
+}
+
 </script>
 
 <template>
@@ -16,18 +33,21 @@ import PrimaryBtn from '../../Components/PrimaryBtn.vue';
             </p>
         </div>
 
-        <form class="space-y-6">
+        <!-- Error Messages -->
+         <ErrorMessages :errors="form.errors"/>
 
-            <inputField label="Nome" icon="id-badge"/>
-            <inputField label="E-Mail" type="email" icon="at"/>
-            <inputField label="Password" type="password" icon="key"/>
-            <inputField label="Confirm Password" type="password" icon="key"/>
+        <form @submit.prevent="submit" class="space-y-6">
+
+            <inputField label="Nome" icon="id-badge" v-model="form.name"/>
+            <inputField label="E-Mail" type="email" icon="at" v-model="form.email"/>
+            <inputField label="Password" type="password" icon="key" v-model="form.password"/>
+            <inputField label="Confirm Password" type="password" icon="key" v-model="form.password_confirmation"/>
 
             <p class="text-slate-500 text-sm dark:text-slate-400">
                 Ao clicar em registrar, concorda com os <TextLink routeName="home" label="Termos de Serviço"/>
             </p>
 
-            <PrimaryBtn>Registrar</PrimaryBtn>
+            <PrimaryBtn :disabled="form.processing">Registrar</PrimaryBtn>
 
         </form>
     </Container>
